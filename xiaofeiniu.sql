f0df1aab2b23e32d802b00f94e9baa1fdb22d016
Song Song <13880072191@163.com>
2019-01-25 16:04:57 +0800

    小肥牛数据库

---
+++ xiaofeiniu.sql
@@ -0,0 +1,12 @@
+SET NAMES UTF8;
+DROP DATABASE IF EXISTS xiaofeiniu;
+CREATE DATABASE xiaofeiniu CHARSET=UTF8;
+USE xiaofeiniu;
+
+CREATE TABLE xfn_admin(
+    aid INT PRIMARY KEY AUTO_INCREMENT,
+    aname VARCHAR(32) UNIQUE,
+    apwd VARCHAR(64)
+);
+INSERT INTO xfn_admin VALUES(NULL,'admin','123456');
+INSERT INTO xfn_admin VALUES(NULL,'boss','999999');
